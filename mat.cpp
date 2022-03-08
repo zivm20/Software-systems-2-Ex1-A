af4d2ae068142653510e8bd55cf601ee95d06a9f
Ziv Morgan <67800437+zivm20@users.noreply.github.com>
2022-03-08 17:42:12 +0200

    Fixed bugs

---
+++ mat.cpp
@@ -0,0 +1,68 @@
+#include "mat.hpp"
+#include  <iostream>
+using namespace std;
+
+namespace ariel{
+
+	string mat(int w, int h, char c1, char c2){
+		if(w<0 || h<0)
+			throw std::invalid_argument("Width and height must be positive");
+		else if(w%2==0 || h%2==0)
+			throw std::invalid_argument("Width and height must be odd");
+		string str1 = "";
+		string str2 = "";
+		for(int i = 0; i<w; i++){
+			str1 = str1 + c1;
+			str2 = str2 + c2;
+		}
+		
+		int rowsDone = 0;
+		string top = "";
+		string bot = "";
+		bool line = true;
+		
+		for(int i = 0; i<(h-1)/2; i++){
+			string s = "";
+			if(line)
+				s = str1;
+			else
+				s = str2;
+				
+			top = top + s + "\n";
+			if(i != 0)
+				bot = s + "\n"+ bot;
+			else
+				bot = s + "\n";
+				
+			if(i<(w+1)/2){
+				if(line){
+					str2[i] = c1;
+					str2[w-1-i] = c1;
+				}
+				else{
+					str1[i] = c2;
+					str1[w-1-i] = c2;
+				}
+			}
+			rowsDone += 1;
+			line = !line;
+		}
+		
+		string s = "";
+		if(line)
+			s = str1;
+		else
+			s = str2;
+		top = top + s + "\n";
+		rowsDone += 1;
+		
+	
+	return top + bot;
+	
+	}
+
+}
+
+
+
+
