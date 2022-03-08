b95c85b1e34f7f88dd6f90469e34f88dea4ce3bd
Ziv Morgan <67800437+zivm20@users.noreply.github.com>
2022-03-08 14:51:21 +0200

    Wrote code

---
+++ TestExample.cpp
@@ -0,0 +1,50 @@
+/**
+ * An example of how to write unit tests.
+ * Use this as a basis to build a more complete Test.cpp file.
+ * 
+ * IMPORTANT: Please write more tests - the tests here are only for example and are not complete.
+ *
+ * AUTHORS: <Please write your names here>
+ * 
+ * Date: 2021-02
+ */
+
+#include "doctest.h"
+#include "mat.hpp"
+using namespace ariel;
+
+#include <string>
+#include <algorithm>
+using namespace std;
+
+/**
+ * Returns the input string without the whitespace characters: space, newline and tab.
+ * Requires std=c++2a.
+ */
+string nospaces(string input) {
+	std::erase(input, ' ');
+	std::erase(input, '\t');
+	std::erase(input, '\n');
+	std::erase(input, '\r');
+	return input;
+}
+
+
+TEST_CASE("Good input") {
+	CHECK(nospaces(mat(9, 7, '@', '-')) == nospaces("@@@@@@@@@\n"
+													 "@-------@\n"
+													 "@-@@@@@-@\n"
+													 "@-@---@-@\n"
+													 "@-@@@@@-@\n"
+													 "@-------@\n"
+													 "@@@@@@@@@"));
+	/* Add more test here */
+}
+
+TEST_CASE("Bad input") {
+    CHECK_THROWS(mat(10, 5, '$', '%'));
+    /* Add more test here */
+}
+
+
+/* Add more test cases here */
